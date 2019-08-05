7e07fadb6efd8aca8003fbac9d210e0403354777
Simon R. Glenn <simonrglenn@gmail.com>
2019-08-05 16:35:59 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do 
+        'My name is Simon.'
+    end
+
+    get '/hometown' do 
+        'My hometown is Manhattan.'
+    end
+
+    get '/favorite-song' do 
+        'My favorite song is Banana Phone by Rafi.'
+    end
+
 end
