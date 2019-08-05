83c86011e0eaac7d23219f81e0edbaf774968700
Luis Ascencio <luismiguelascencio@gmail.com>
2019-08-05 08:38:14 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+    get "/name" do 
+        "My name is Luis"
+    end
+
+
+
+
+    get "/hometown" do 
+        "My hometown is Colima. Mexico yo! "
+    end
+
+
+    get '/favorite-song' do 
+        "My favorite song is A Tribe Called Quest - Excursions"
+    end
+
 end
